4595016bbb87b7c6edc7623aef1b5801b662cdaf
EjunYoungKR <junyeong6877@gmail.com>
2023-09-20 23:57:22 +0900

    git test

---
+++ for_git/target/debug/deps/for_git-83b1a2c397c33dbc.d
@@ -0,0 +1,5 @@
+/Users/junyoung_lee/rust_man/for_git/target/debug/deps/for_git-83b1a2c397c33dbc: src/main.rs
+
+/Users/junyoung_lee/rust_man/for_git/target/debug/deps/for_git-83b1a2c397c33dbc.d: src/main.rs
+
+src/main.rs:
---
+++ for_git/target/debug/for_git.d
@@ -0,0 +1,1 @@
+/Users/junyoung_lee/rust_man/for_git/target/debug/for_git: /Users/junyoung_lee/rust_man/for_git/src/main.rs
